18be5221361d6cc5efcf4091d4f326704666e6e6
natalie <kimnatalie01@gmail.com>
2024-01-09 22:05:37 -0500

    24-01-09: make basic template

---
+++ bay-ridge.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="stylesheet.css">
+
+  <title>Bay Ridge Youth March by nationalkim</title>
+</head>
+
+<body>
+  <!-- header -->
+  <?php include "header.php" ?>
+
+  <!-- main -->
+  <div id="main">
+    <img src="P1110135.JPG" alt="man on bridge">
+    <img src="P1110067.JPG" alt="man on bridge">
+    <img src="P1110202.JPG" alt="man on bridge">
+    <img src="P1110225.JPG" alt="man on bridge">
+  </div>
+
+  <!--footer -->
+</body>
+</html>
